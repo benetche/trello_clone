94d79667a86a6d7c37bd90133b5c6d55d0e452c7
Vítor Beneti Martins <vitor.beneti@gmail.com>
2022-11-25 18:14:00 -0300

    adding some styles and drag and drop sensation

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,5 +1,21 @@
 import styled from "styled-components";
 
-export const CardContainer = styled.div``;
+export const CardContainer = styled.div`
+  position: relative;
+  background-color: #fff;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  padding: 20px;
+  box-shadow: 0 1px 4px 0 rgba(192, 208, 230, 0.8);
+  cursor: grab; /* adding a "drag and drop" sensation */
+
+  header {
+    position: absolute;
+  }
+
+  p {
+    font-weight: 500px;
+  }
+`;
 
 export const CardLabel = styled.span``;
