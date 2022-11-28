9fe0eec0ee4a389677b6805382bc27893f0584de
Vítor Beneti Martins <vitor.beneti@gmail.com>
2022-11-28 01:08:22 -0300

    drag and drop api included

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import GlobalStyles from "./styles/global";
 import Header from "./components/Header";
 import Table from "./components/Table";
-// import { DndProvider } from "react-dnd";
-// import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <Table />
       <GlobalStyles />
-    </>
+    </DndProvider>
   );
 }
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { CardContainer, CardLabel } from "./styles";
+import { useDrag } from "react-dnd";
 
 export default function Card({ data }) {
+  const [{ isDragging }, dragRef] = useDrag({
+    type: "CARD",
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
   return (
-    <CardContainer>
+    <CardContainer ref={dragRef}>
       <header>
         <h2>Titulo</h2>
         {data.labels.map((label) => (
           <CardLabel key={label} color={label} />
         ))}
       </header>
-      <p>Teste conteudo</p>
+      <p>{data.content}</p>
     </CardContainer>
   );
 }
