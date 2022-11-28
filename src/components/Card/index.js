9bbd481df731997788d7ad338e5be2c47b6b60f9
Vítor Beneti Martins <vitor.beneti@gmail.com>
2022-11-28 04:14:38 -0300

    responsive card on hover

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { CardContainer, CardLabel } from "./styles";
+import { CardContainer } from "./styles";
 import { useDrag } from "react-dnd";
+import { Button, Checkbox, FormControlLabel } from "@mui/material";
+import { MdDelete } from "react-icons/md";
 
 export default function Card({ data }) {
-  const [{ isDragging }, dragRef] = useDrag({
-    type: "CARD",
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  });
+  // const [{ isDragging }, dragRef] = useDrag({
+  //   type: "CARD",
+  //   collect: (monitor) => ({
+  //     isDragging: monitor.isDragging(),
+  //   }),
+  // });
 
   return (
-    <CardContainer ref={dragRef} isDragging={isDragging}>
-      <p>{data.content}</p>
+    <CardContainer>
+      <div id="content">
+        <p>{data.content}</p>
+        <button id="delete" size="small">
+          <MdDelete />
+        </button>
+      </div>
+      <footer>
+        <FormControlLabel
+          control={<Checkbox />}
+          label="Done"
+        ></FormControlLabel>
+      </footer>
     </CardContainer>
   );
 }
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -4,19 +4,51 @@ export const CardContainer = styled.div`
   background-color: #fff;
   border-radius: 5px;
   margin-bottom: 20px;
-  padding: 20px;
+  padding: 15px;
   box-shadow: 0 1px 4px 0 rgba(192, 208, 230, 0.8);
-  cursor: grab; /* adding a "drag and drop" sensation */
+  /* cursor: grab; adding a "drag and drop" sensation */
+
+  &:hover {
+    transition: ease-in 0.2s;
+    filter: brightness(95%);
+  }
+
+  &:hover #content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  #delete {
+    display: none;
+  }
+  #delete:hover {
+    filter: brightness(95%);
+  }
+
+  &:hover #delete {
+    width: 30px;
+    height: 30px;
+    border-radius: 5px;
+    display: inline-block;
+    position: absolute;
+    right: 15px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    background: #fff;
+    cursor: pointer;
+  }
 
   p {
-    font-weight: 400;
+    font-weight: bold;
+    font-size: 16px;
   }
 
-  header {
+  footer {
     font-weight: 500;
+    padding: 0;
   }
 
-  ${(props) =>
+  /* ${(props) =>
     props.isDragging &&
     css`
       border: 1px dashed rgba(0, 0, 0, 0.2);
@@ -28,13 +60,5 @@ export const CardContainer = styled.div`
       * {
         opacity: 0;
       }
-    `}
-`;
-
-export const CardLabel = styled.span`
-  width: 10px;
-  height: 10px;
-  border-radius: 2px;
-  display: inline-block;
-  background: ${(props) => props.color};
+    `} */
 `;
