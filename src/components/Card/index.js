81af866422b15570b5a011a06678fa5311a578d6
Vítor Beneti Martins <vitor.beneti@gmail.com>
2022-11-28 00:56:07 -0300

    card finished

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { CardContainer, CardLabel } from "./styles";
 
-export default function Card() {
+export default function Card({ data }) {
   return (
     <CardContainer>
       <header>
         <h2>Titulo</h2>
-        <CardLabel color="red" />
+        {data.labels.map((label) => (
+          <CardLabel key={label} color={label} />
+        ))}
       </header>
       <p>Teste conteudo</p>
     </CardContainer>
